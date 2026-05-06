77532146857af748f724a5dfdd446d0fb1ded33d
IbnAbubakri <faruqsuzay@gmail.com>
2026-05-06 20:40:22 +0100

    Add laptop favicon with AFA on screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script
           dangerouslySetInnerHTML={{
