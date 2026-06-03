d810e131bcf47dbf189790686a476c3b5247fb3e
IbnAbubakri <faruqsuzay@gmail.com>
2026-06-03 20:43:30 +0100

    Fix imports: use framer-motion instead of motion package

--- src/components/TiltCard.tsx
+++ src/components/TiltCard.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useRef, useState } from 'react'
-import { motion } from 'motion/react'
+import { motion } from 'framer-motion'
 
 interface TiltCardProps {
   children: React.ReactNode
